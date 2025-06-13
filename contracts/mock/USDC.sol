ecae09afa21c9863c1c36bdc7b7eda008c4bc8e1
LendefiMarkets <alexei@lendefimarkets.com>
2025-06-13 13:01:49 -0700

    port USDC to Binance

--- contracts/mock/USDC.sol
+++ contracts/mock/USDC.sol
@@ -6,8 +6,12 @@ import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 contract USDC is ERC20 {
     constructor() ERC20("USD Coin", "USDC") {}
 
+    function decimals() public pure override returns (uint8) {
+        return 18;
+    }
+
     function drip(address to) public {
-        _mint(to, 20000e6);
+        _mint(to, 20000e18);
     }
 
     function mint(address to, uint256 amount) public {
