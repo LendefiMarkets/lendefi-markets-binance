dab931319df37b9df7438f0c3bcddc3dc7dd7bee
LendefiMarkets <alexei@lendefimarkets.com>
2025-06-16 21:04:09 -0700

    forge format

--- contracts/interfaces/ITeamVesting.sol
+++ contracts/interfaces/ITeamVesting.sol
@@ -5,7 +5,6 @@ pragma solidity 0.8.23;
  * @title Team Vesting Interface
  * @custom:security-contact security@lendefimarkets.com
  */
-
 interface ITEAMVESTING {
     /**
      * @dev Cancelled Event
