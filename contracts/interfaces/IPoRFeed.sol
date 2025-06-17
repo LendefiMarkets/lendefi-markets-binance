96e94d0310f3080655782b26a654980ca849bb54
LendefiMarkets <alexei@lendefimarkets.com>
2025-06-16 21:03:53 -0700

    forge format

--- contracts/interfaces/IPoRFeed.sol
+++ contracts/interfaces/IPoRFeed.sol
@@ -2,11 +2,7 @@
 pragma solidity 0.8.23;
 
 interface IPoRFeed {
-    function initialize(
-        address _asset,
-        address _updater,
-        address _owner
-    ) external;
+    function initialize(address _asset, address _updater, address _owner) external;
 
     function decimals() external view returns (uint8);
 
@@ -14,29 +10,15 @@ interface IPoRFeed {
 
     function version() external view returns (uint256);
 
-    function getRoundData(
-        uint80 _roundId
-    )
+    function getRoundData(uint80 _roundId)
         external
         view
-        returns (
-            uint80 roundId,
-            int256 answer,
-            uint256 startedAt,
-            uint256 updatedAt,
-            uint80 answeredInRound
-        );
+        returns (uint80 roundId, int256 answer, uint256 startedAt, uint256 updatedAt, uint80 answeredInRound);
 
     function latestRoundData()
         external
         view
-        returns (
-            uint80 roundId,
-            int256 answer,
-            uint256 startedAt,
-            uint256 updatedAt,
-            uint80 answeredInRound
-        );
+        returns (uint80 roundId, int256 answer, uint256 startedAt, uint256 updatedAt, uint80 answeredInRound);
 
     function updateAnswer(uint80 _roundId, int256 _answer) external;
 
