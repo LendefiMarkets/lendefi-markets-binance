14efbf6d1c654b523ccc42a6f3f87607a76791b3
LendefiMarkets <alexei@lendefimarkets.com>
2025-06-16 21:10:57 -0700

    forge format

--- test/fork-tests/UsdSymbolDetectionTest.sol
+++ test/fork-tests/UsdSymbolDetectionTest.sol
@@ -18,7 +18,7 @@ contract UsdSymbolDetectionTest is Test {
     address public constant USDC = 0x8AC76a51cc950d9822D68b83fE1Ad97B32Cd580d;
     address public constant CAKE = 0x0E09FaBB73Bd3Ade0a17ECC321fD13a19e81cE82;
     address public constant LINK = 0xF8A0BF9cF54Bb92F17374d9e9A321E6a111a51bD;
-    
+
     // Pools
     address public constant WBNB_USDT_POOL = 0x36696169C63e42cd08ce11f5deeBbCeBae652050;
     address public constant WBNB_USDC_POOL = 0xf2688Fb5B81049DFB7703aDa5e770543770612C4;
@@ -31,7 +31,7 @@ contract UsdSymbolDetectionTest is Test {
 
     function test_UsdSymbolDetection() public view {
         console2.log("=== USD Symbol Detection Test ===");
-        
+
         // Test individual token symbols
         console2.log("\n--- Individual Token Symbols ---");
         _testTokenSymbol(WBNB, "WBNB");
@@ -39,7 +39,7 @@ contract UsdSymbolDetectionTest is Test {
         _testTokenSymbol(USDC, "USDC");
         _testTokenSymbol(CAKE, "CAKE");
         _testTokenSymbol(LINK, "LINK");
-        
+
         // Test pool analysis
         console2.log("\n--- Pool Analysis ---");
         _testPoolForUSD(WBNB_USDT_POOL, "WBNB/USDT");
@@ -58,23 +58,23 @@ contract UsdSymbolDetectionTest is Test {
         IPancakeV3Pool pool = IPancakeV3Pool(poolAddress);
         address token0 = pool.token0();
         address token1 = pool.token1();
-        
+
         console2.log(string.concat("\n", poolName, " Pool:"));
         console2.log("Token0:", token0);
         console2.log("Token1:", token1);
-        
+
         // Check if WBNB is in pool
         bool hasWBNB = (token0 == WBNB || token1 == WBNB);
         console2.log("Has WBNB:", hasWBNB);
-        
+
         if (hasWBNB) {
             address otherToken = (token0 == WBNB) ? token1 : token0;
             string memory otherSymbol = IERC20Metadata(otherToken).symbol();
             bool isDirectUSDPool = _containsUSD(otherSymbol);
-            
+
             console2.log(string.concat("Other token symbol: '", otherSymbol, "'"));
             console2.log("Is direct USD pool:", isDirectUSDPool);
-            
+
             if (isDirectUSDPool) {
                 console2.log("-> Should use direct USD pricing");
             } else {
@@ -93,18 +93,19 @@ contract UsdSymbolDetectionTest is Test {
     function _containsUSD(string memory symbol) internal pure returns (bool) {
         bytes memory symbolBytes = bytes(symbol);
         bytes3 usdPattern = "USD";
-        
+
         if (symbolBytes.length < 3) return false;
-        
+
         // Check each position for "USD" pattern
         for (uint256 i = 0; i <= symbolBytes.length - 3; i++) {
-            if (symbolBytes[i] == usdPattern[0] && 
-                symbolBytes[i + 1] == usdPattern[1] && 
-                symbolBytes[i + 2] == usdPattern[2]) {
+            if (
+                symbolBytes[i] == usdPattern[0] && symbolBytes[i + 1] == usdPattern[1]
+                    && symbolBytes[i + 2] == usdPattern[2]
+            ) {
                 return true;
             }
         }
-        
+
         return false;
     }
 }
